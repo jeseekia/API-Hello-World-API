61d603bb3b99109759693fe93403eb22df4a075c
Jeseekia Vaughn <jeseekia@gmail.com>
2023-09-10 13:41:58 -0400

    Feature: Seed, list and show languages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const mongoose = require('mongoose')
+const languages = require('./controllers/languages_controller')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -17,6 +18,10 @@ app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.send('Welcome to Jeseekia\'s Hello World! API')
 })
+
+// LANGUAGES
+const languagesController = require('./controllers/languages_controller')
+app.use('/languages', languagesController)
 
 // LISTEN
 app.listen(PORT, () => {
